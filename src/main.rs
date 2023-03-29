1d3eb0aba795eadd2f18274ff4866cb714359ad2
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-03-29 21:02:27 +0700

    Более-менее приличный сервер

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpServer};
+
+mod app_state;
+mod services;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let factory = || {
+        App::new()
+            .configure(services::setup_services)
+            .app_data(web::Data::new(app_state::AppState::new()))
+    };
+
+    let server = HttpServer::new(factory).bind(("127.0.0.1", 80))?;
+    return server.run().await;
+}
